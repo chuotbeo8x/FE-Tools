f0e9b2810c340394082025e69a5097440c237733
chuotbeo8x <hoanggiap@gmail.com>
2022-05-19 16:42:52 +0700

    19052022

---
+++ src/assets/js/dev/inputclear.js
@@ -0,0 +1,20 @@
+//======================== Input Clear ========================//
+$(document).ready(function() {
+	//input clear
+	$('.input-ic-clear').on('mousedown', function () {
+		var input = $(this).closest('.input-inner-wrap').find('.input');
+		input.val('')
+		input.trigger('input');
+	})
+	$('.input-has-clear').on('paste keyup change', function () {
+		var inputClear = $(this).closest('.input-inner-wrap').find('.input-ic-clear');
+			if ($(this).val()) {
+		inputClear.addClass('show');
+		}
+		else {
+			inputClear.removeClass('show');
+		}
+	});
+	//end input clear
+});
+//======================== Input Clear ========================//
